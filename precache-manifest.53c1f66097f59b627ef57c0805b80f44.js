882f08da0d946a4363563d2a8cbae71145686c01
Liliia <chuiko1112@gmail.com>
2020-07-08 21:56:39 +0300

    Updates

---
+++ precache-manifest.53c1f66097f59b627ef57c0805b80f44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff95a24e829e4a0217b63e507e76aec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32d22a76596725a025dc",
+    "url": "./static/css/main.829808d5.chunk.css"
+  },
+  {
+    "revision": "67af20f78517dd510f76",
+    "url": "./static/js/2.bb8298db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bb8298db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d22a76596725a025dc",
+    "url": "./static/js/main.6ca20745.chunk.js"
+  },
+  {
+    "revision": "93b65d15acd366da5855",
+    "url": "./static/js/runtime-main.2badbef3.js"
+  }
+]);
